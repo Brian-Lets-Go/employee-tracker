d2ac1159450d681bc65a8b854dceedcfa70c6fbc
brian-lets-go <brian.w.meenan@gmail.com>
2022-03-13 00:51:14 -0500

    updated java

--- server.js
+++ server.js
@@ -1,36 +1,241 @@
+//dependencies
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
-const console = require("console.table");
+const consol = require("console.table");
 
-// const PORT = 3001;
+//Create mysql connection
+const connect = mysql.createConnection({
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'newPassword',
+    database: 'tracker'
+});
 
-// const db = mysql.createConnection({
-//     host: 'localhost',
-//     port: 3006,
-//     // Your MySQL username,
-//     user: 'root',
-//     // Your MySQL password
-//     password: 'newPassword',
-//     database: 'tracker'
-//   });
-
-// // Start server after DB connection
-// db.connect(err => {
-//     if (err) throw err;
-//     start();
-// });
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "newPassword"
-  });
-  
-  con.connect(function(err) {
+// Start server after DB connection
+connect.connect(err => {
     if (err) throw err;
-    console.log("Connected!");
-  });
+    console.log('Database connected.');
+    start();
+});
 
-// function start() {
-//     console.log("STARTED!");
-// }
+
+function start() {
+    console.log("STARTED!");
+
+    inquirer.prompt({
+      type: "list",
+      name: "action",
+      message: "Choose one of the below:",
+      choices: [
+            "View Departments",
+            "View Roles",
+            "View Employees",
+            "Add a Department",
+            "Add a Role",
+            "Add an Employee",
+            "Update Employee Role",
+            "Finish"
+        ]
+    }).then(function ({action}) {
+        switch (action) {
+
+            case "View Departments":
+                viewDepartments();
+                break;
+
+            case "View Roles":
+                viewRoles();
+                break;
+            
+            case "View Employees":
+                viewEmployees();
+                break;
+            
+            case "Add a Department":
+                addDepartment();
+                break;
+            
+            case "Add a Role":
+                addRole();
+                break;
+            
+            case "Add an Employee":
+                addEmployee();
+                break
+            
+            case "Update Employee Role":
+                updateEmployee();
+                break
+            
+            case "Finish":
+                connect.end();
+                break
+        }
+    })
+}
+
+function viewDepartments() {
+    
+    var query = `SELECT * FROM department`;
+
+    connect.query(query, function (err, res) {
+        if (err) throw err;
+
+        console.table(res);
+        console.log("Departments")
+        
+    start();
+    
+    }
+)}
+
+function viewRoles() {
+    
+    var query = `SELECT * FROM role
+                    LEFT JOIN department
+                    ON role.department_id = department.id`;
+
+    connect.query(query, function (err, res) {
+        if (err) throw err;
+
+        console.table(res);
+        console.log("Departments")
+        
+    start();
+    
+    }
+)}
+
+function viewEmployees() {
+
+    var query = `SELECT * FROM employee
+                    LEFT JOIN role
+                    ON employee.role_id = role.id
+                    LEFT JOIN department
+                    ON department.id = role.department_id`;
+
+    connect.query(query, function (err, res) {
+        if (err) throw err;
+
+        console.table(res);
+        console.log("Departments")
+        
+    start();
+    
+    }
+)}
+
+function addDepartment() {
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "department_name",
+            message: "What department do you want to add?"
+        }
+
+    ]).then(function (answer) {
+        
+        var query = `INSERT INTO department SET ?`
+
+        connect.query(query,
+
+            {name: answer.department_name},
+        
+            function (err, res) {
+            
+            if (err) throw err;
+            console.table(res);
+
+            start();
+
+        })
+    })
+}
+
+function addRole() {
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "role_name",
+            message: "What role do you want to add?"
+        },
+        {
+            type: "input",
+            name: "role_salary",
+            message: "What is the salary for this role?"
+        },
+        {
+           type: "input",
+           name: "role_department_id",
+           message: "What is the department ID?"
+        },
+
+    ]).then(function (answer) {
+        
+        var query = `INSERT INTO role SET ?`
+
+        connect.query(query,
+
+            {
+                title: answer.role_name,
+                salary: answer.role_salary,
+                department_id: answer.role_department_id
+            },
+        
+            function (err, res) {
+            
+            if (err) throw err;
+            console.table(res);
+
+            start();
+
+        })
+    })
+}
+
+function addEmployee() {
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "first_name",
+            message: "What is the employee's first name?"
+        },
+        {
+            type: "input",
+            name: "last_name",
+            message: "What is the employee's last name?"
+        },
+        {
+           type: "input",
+           name: "role_id",
+           message: "What is the employee's role ID?"
+        },
+    ]).then(function (answer) {
+        
+        var query = `INSERT INTO employee SET ?`
+
+        connect.query(query,
+
+            {
+                first_name: answer.first_name,
+                last_name: answer.last_name,
+                role_id: answer.role_id,
+            },
+        
+            function (err, res) {
+            
+            if (err) throw err;
+            console.table(res);
+
+            start();
+
+        })
+    })
+}
+
+
